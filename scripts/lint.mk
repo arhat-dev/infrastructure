38c33ef1b06b8364e2b9d65d50d462c736242e72
Jeffrey Stoke <me@arhat.dev>
2021-02-01 01:15:47 +0100

    Bump sample charts version
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- scripts/lint.mk
+++ scripts/lint.mk
@@ -15,11 +15,15 @@
 RUN_LINTER := docker run -t --rm -v "$(shell pwd):$(shell pwd)" -w "$(shell pwd)"
 
 lint.file:
-	${RUN_LINTER} mstruebing/editorconfig-checker:2.2.0 ec -config .ecrc
+	${RUN_LINTER} mstruebing/editorconfig-checker:2.3.1 ec -config .ecrc
 
 lint.shell:
 	${RUN_LINTER} koalaman/shellcheck-alpine:stable \
-		sh -c "find . | grep -E -e '.sh\$$' | grep -v build | grep -v charts/ | xargs -I'{}' shellcheck -S warning -e SC1090 -e SC1091 {} ;"
+		sh -c "find . \
+		| grep -E -e '.sh\$$' \
+		| grep -v build \
+		| grep -v charts/ \
+		| xargs -I'{}' shellcheck -S warning -e SC1090 -e SC1091 {} ;"
 
 lint.yaml:
 	${RUN_LINTER} arhatdev/yamllint:latest yamllint -c .yaml-lint.yml .
